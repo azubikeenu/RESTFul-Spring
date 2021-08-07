c409cdcd9fc2bb194947091ecc59e133e9f409cb
Enu Azubike <enuazubike88@gmail.com>
2021-08-07 04:11:56 +0100

    reading tokens from applications.properties

--- src/main/java/com/azubike/ellpisis/app/ws/security/SecurityConstants.java
+++ src/main/java/com/azubike/ellpisis/app/ws/security/SecurityConstants.java
@@ -1,10 +1,17 @@
 package com.azubike.ellpisis.app.ws.security;
 
+import com.azubike.ellpisis.app.ws.SpringApplicationContext;
+
 public class SecurityConstants {
 	public static final long EXPIRATION_TIME = 864000000; // 10 days
 	public static final String TOKEN_PREFIX = "Bearer:";
 	public static final String HEADER_STRING = "Authorization";
 	public static final String SIGN_UP_URL = "/users";
 	public static final String TOKEN_SECRET = "j6xwyvChZm0j41";
+
+	public static String getTokenSecret() {
+		AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("appProperties");
+		return appProperties.getTokenSecret();
+	}
 
 }
