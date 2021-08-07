5030377fd23fb84bb8080b31a45d69d6f192cec1
Enu Azubike <enuazubike88@gmail.com>
2021-08-07 03:57:53 +0100

    Implemented Stateless Authorization Filter

--- src/main/java/com/azubike/ellpisis/app/ws/security/SecurityConstants.java
+++ src/main/java/com/azubike/ellpisis/app/ws/security/SecurityConstants.java
@@ -5,6 +5,6 @@ public class SecurityConstants {
 	public static final String TOKEN_PREFIX = "Bearer:";
 	public static final String HEADER_STRING = "Authorization";
 	public static final String SIGN_UP_URL = "/users";
-	public static final String TOKEN_SECRET = "jf9ty12598hjtyk";
+	public static final String TOKEN_SECRET = "j6xwyvChZm0j41";
 
 }
