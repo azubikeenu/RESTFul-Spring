26fe8a4cd172be8901f6c15f8a2e54a3efba7162
Enu Azubike <enuazubike88@gmail.com>
2021-08-22 04:05:42 +0100

    Making email verification public

--- src/main/java/com/azubike/ellpisis/app/ws/security/SecurityConstants.java
+++ src/main/java/com/azubike/ellpisis/app/ws/security/SecurityConstants.java
@@ -8,6 +8,7 @@ public class SecurityConstants {
 	public static final String HEADER_STRING = "Authorization";
 	public static final String SIGN_UP_URL = "/users";
 	public static final String TOKEN_SECRET = "j6xwyvChZm0j41";
+	public static final String ENAIL_VERIFICATION_URL = "/users/email-verification";
 
 	public static String getTokenSecret() {
 		AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("appProperties");
