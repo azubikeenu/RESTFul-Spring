65f980d84274e8ec683598859afe41125bee573c
Enu Azubike <enuazubike88@gmail.com>
2021-09-12 03:39:37 +0100

    implemented test cases for createUser

--- src/test/java/com/azubike/ellpisis/app/ws/service/impl/UserServiceImplTest.java
+++ src/test/java/com/azubike/ellpisis/app/ws/service/impl/UserServiceImplTest.java
@@ -2,18 +2,35 @@ package com.azubike.ellpisis.app.ws.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.azubike.ellpisis.app.ws.exceptions.UserServiceException;
+import com.azubike.ellpisis.app.ws.io.entity.AddressEntity;
 import com.azubike.ellpisis.app.ws.io.entity.UserEntity;
 import com.azubike.ellpisis.app.ws.repo.UserRepository;
+import com.azubike.ellpisis.app.ws.shared.dto.AddressDto;
 import com.azubike.ellpisis.app.ws.shared.dto.UserDto;
+import com.azubike.ellpisis.app.ws.shared.utils.EmailService;
+import com.azubike.ellpisis.app.ws.shared.utils.Utils;
 
 class UserServiceImplTest {
 	// this is added because the class contains dependencies
@@ -21,23 +38,93 @@ class UserServiceImplTest {
 	UserServiceImpl userServiceImpl;
 	@Mock
 	UserRepository userRepository;
+	@Mock
+	Utils utils;
+	@Mock
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	@Mock
+	EmailService emailService;
+	String userId = "xxssyyusuuwiei";
+	String encryptedPassword = "xxxxwuuurhhrhhuuxbbgdg_eyrnnr";
+	String emailVerificationToken = "xxxeuueuuriixnnxmeuue";
+	UserEntity userEntity;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		MockitoAnnotations.openMocks(this);
+		userEntity = new UserEntity();
+		userEntity.setId(1L);
+		userEntity.setUserId(userId);
+		userEntity.setFirstName("Richard");
+		userEntity.setLastName("Enu");
+		userEntity.setEmail("enazubike88@gmail.com");
+		userEntity.setAddresses(getAddressEntities());
 	}
 
 	@Test
 	void testGetUsers() {
-		UserEntity userEntity = new UserEntity();
-		userEntity.setId(1L);
-		userEntity.setUserId("xxssyyusuuwiei");
-		userEntity.setFirstName("Richard enu");
-		userEntity.setEncryptedPassword("xxxxwuuurhhrhhuuxbbgdg_eyrnnr");
+		userEntity.setEncryptedPassword(encryptedPassword);
 		when(userRepository.findByEmail(anyString())).thenReturn(userEntity);
-		UserDto userDto = userServiceImpl.getUser("enuazubike@gmail.com");
+		UserDto userDto = userServiceImpl.getUser(userEntity.getEmail());
 		assertNotNull(userDto);
-		assertEquals("Richard enu", userDto.getFirstName());
+		assertEquals("Richard", userDto.getFirstName());
+	}
+
+	@Test
+	void testGetUsers_UsernameNotFoundException() {
+		when(userRepository.findByEmail(anyString())).thenReturn(null);
+		assertThrows(UserServiceException.class, () -> {
+			userServiceImpl.getUser(userEntity.getEmail());
+		});
+	}
+
+	@Test
+	final void createUser() {
+		when(userRepository.findByEmail(anyString())).thenReturn(null);
+		when(utils.generateRadomAddressId(anyInt())).thenReturn("xsueuuiriieiieioqoooeooroux");
+		when(utils.generateRandomUserId(anyInt())).thenReturn(userId);
+		when(utils.generateEmailVerificationToken(anyString())).thenReturn(emailVerificationToken);
+		when(bCryptPasswordEncoder.encode(anyString())).thenReturn(encryptedPassword);
+		when(userRepository.save(any(UserEntity.class))).thenReturn(userEntity);
+		UserDto userDto = new UserDto();
+		userDto.setAddresses(getAddressDto());
+		userDto.setFirstName("Richard");
+		userDto.setLastName("Enu");
+		userDto.setEmail("enuazubike88@gmail.com");
+		userDto.setPassword("12345");
+		UserDto returnedUser = userServiceImpl.createUser(userDto);
+		assertNotNull(returnedUser);
+		assertEquals(userEntity.getFirstName(), returnedUser.getFirstName());
+		assertEquals(userEntity.getLastName(), returnedUser.getLastName());
+		assertNotNull(returnedUser.getUserId());
+		assertEquals(returnedUser.getAddresses().size(), userEntity.getAddresses().size());
+		// verify methods
+		verify(utils, times(returnedUser.getAddresses().size())).generateRadomAddressId(30);
+		verify(bCryptPasswordEncoder, times(1)).encode("12345");
+		verify(userRepository, times(1)).save(any(UserEntity.class));
+	}
+
+	private List<AddressDto> getAddressDto() {
+		AddressDto addressOne = new AddressDto();
+		addressOne.setType("shipping");
+		addressOne.setCity("Lagos");
+		addressOne.setCountry("Nigeria");
+		addressOne.setStreetName("Festac");
+		AddressDto addressTwo = new AddressDto();
+		addressTwo.setType("billing");
+		addressTwo.setCity("Lagos");
+		addressTwo.setCountry("Nigeria");
+		addressTwo.setStreetName("Festac");
+		List<AddressDto> addresses = Arrays.asList(addressOne, addressTwo);
+		return addresses;
+
+	}
+
+	private List<AddressEntity> getAddressEntities() {
+		List<AddressDto> addresses = getAddressDto();
+		Type listType = new TypeToken<List<AddressEntity>>() {
+		}.getType();
+		return new ModelMapper().map(addresses, listType);
 	}
 
 }
